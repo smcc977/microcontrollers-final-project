f85ec0fb0187e237d7dd86ddee4d0e9225ad365b
sam <s_mccollough@u.pacific.edu>
2025-11-05 10:24:12 -0800

    Adding change functionality to state machine

--- traffic-light/main.c
+++ traffic-light/main.c
@@ -20,6 +20,10 @@
 #include "lookup.h"
 #include "keypad.h"
 
+#define greenTimeLeft 20
+#define changeTimeLeft 20
+#define initTimeLeft 1
+
 // Values for keypad state.
 enum States {
   INIT, LANE_ODD, LANE_EVEN, LANE_ODD_CHANGE, LANE_EVEN_CHANGE
@@ -38,11 +42,11 @@ void main ( void ) {
 
   // Initialize variable which indicates which LED digit is visible.
   uint8_t display = 0;
-  uint8_t timeLeft = 10;
-  uint8_t displayTime = 10;
-  bool button1.3 = false;
-  bool button2.4 = false;
-  bool nextGreen1.3 = true;
+  uint8_t timeLeft = 1;
+  uint8_t displayTime = 0;
+  bool button13 = false;
+  bool button24 = false;
+  bool nextGreen13 = true;
 
   // Assume initially that no keys are pressed.
   enum States state = INIT;
@@ -57,19 +61,36 @@ void main ( void ) {
         case INIT:
             //set all lights to red
             //should last one second
-            //switch depending on nextGreen1.3
+            if ( timeLeft < 1 ) {
+                timeLeft = greenTimeLeft;
+                //switch depending on nextGreen1.3
+                if ( nextGreen13 ) {
+                    state = LANE_ODD;
+                }
+                else {
+                    state = LANE_EVEN;
+                }
+            }
             break;
         case LANE_ODD:
             //1+3 green, 2+4 red
             //time left = 20 seconds if button not pressed
             //if button pressed should be set to 7 seconds, reset flag
             //switch to lane_odd_change when time out
+            if ( timeLeft < 1 ) {
+                timeLeft = changeTimeLeft;
+                state = LANE_ODD_CHANGE;
+            }
             break;
         case LANE_EVEN:
             //2+4 green, 1+3 red
             //time left = 20 seconds if button not pressed
             //if button pressed should be set to 7 seconds, reset flag
             //switch to lane_even_change when time out
+            if ( timeLeft < 1 ) {
+                 timeLeft = changeTimeLeft;
+                 state = LANE_EVEN_CHANGE;
+            }
             break;
         case LANE_ODD_CHANGE:
             //1+3 green, 2+4 red
@@ -77,6 +98,10 @@ void main ( void ) {
             //blink the display value or just hold at 0
             //when timeleft == 3: 1+3 yellow
             //switch to init when time out
+            if ( timeLeft < 1 ) {
+                 timeLeft = initTimeLeft;
+                 state = INIT;
+            }
             break;
         case LANE_EVEN_CHANGE:
             //1+3 green, 2+4 red
@@ -84,6 +109,10 @@ void main ( void ) {
             //blink the display value or just hold at 0
             //when timeleft == 3: 1+3 yellow
             //switch to init when time out
+            if ( timeLeft < 1 ) {
+                  timeLeft = initTimeLeft;
+                  state = INIT;
+            }
             break;
     }
 
